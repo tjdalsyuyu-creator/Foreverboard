e16d18474e96d18bcf2e0e144deefe2c85ce4c2d
tjdalsyuyu-creator <tjdalsyuyu@gmail.com>
2026-02-09 13:19:29 +0900

    js/constants.js 만들기

---
+++ js/constants.js
@@ -0,0 +1,18 @@
+// js/constants.js v1.6.5
+export const VERSION = "1.6.5";
+
+export const LS = {
+  UI_TOPBAR_HIDDEN: "mjp_ui_topbar_hidden_v1",
+  AUTO_HIDE_TOPBAR_IN_FS: "mjp_auto_hide_topbar_in_fs_v1",
+
+  SCHEMA: "mjp_v16_schema",
+  RUNTIME: "mjp_v16_runtime",
+  RULESETS: "mjp_v16_rulesets",
+  ACTIVE_RULESET_ID: "mjp_v16_active_ruleset_id",
+  HANDS_PLANS: "mjp_v16_hands_plans",
+};
+
+export const SCHEMA_VERSION = 2;
+
+export const SEATS = [0,1,2,3];
+export const DEFAULT_NAMES = ["동","남","서","북"];
